58238e8c9de400854175db6e156f0c2ea33a8036
soso2405 <55481553+soso2405@users.noreply.github.com>
2019-10-11 09:39:33 +0200

    lache un com

--- single.php
+++ single.php
@@ -14,6 +14,10 @@
         <div><?php the_content();?></div>
         <p> Publié le <?php the_time();?> par <?php the_author_posts_link();?></p>
         <div><p> Cet article a <?php comments_number('aucun commentaire', 'un commentaire', '% commentaires');?></p>
+        <?php if(comments_open() || get_comments_number())
+        {
+          comments_template();
+        }?>
   </article>
 
 <?php endwhile;?> <!-- équiavalent à l'accolade-->
