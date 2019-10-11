a00e08cc44a66ef531f9ac3c7ac571a72e95c5c8
soso2405 <55481553+soso2405@users.noreply.github.com>
2019-10-11 09:34:49 +0200

    commentaire

--- single.php
+++ single.php
@@ -12,7 +12,8 @@
   <article>
         <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
         <div><?php the_content();?></div>
-        <p> Publié le <?php the_time();?> par <?php the_author_posts_link();?></p>  
+        <p> Publié le <?php the_time();?> par <?php the_author_posts_link();?></p>
+        <div><p> Cet article a <?php comments_number('aucun commentaire', 'un commentaire', '% commentaires');?></p>
   </article>
 
 <?php endwhile;?> <!-- équiavalent à l'accolade-->
