af940d37cd7457aecd6ffb1ad7591349d4d339c2
soso2405 <55481553+soso2405@users.noreply.github.com>
2019-10-11 08:53:39 +0200

    wololo

--- header.php
+++ header.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/style.css" />
 	<?php wp_head();?>
-
-	<title><?php bloginfo('name');?>-<?php bloginfo('description');?></title>
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/style.css"/>
-
-
 </head>
 <body>
 	<header>
 		<h1> <?php bloginfo('name');?></h1>
 		<h2> <?php bloginfo('description');?></h2>
+		<nav><?php wp_nav_menu(array('theme_location'=>'menu-principal',
+														'container_class' =>'menu-principal',
 
+								));?></nav>
 	</header>
-
