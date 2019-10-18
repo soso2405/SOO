03f245db4c96436ec5ff7ee08c66f45bee90b6ca
Eliiiise <balvalloire@gmail.com>
2019-10-18 13:39:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php wp_redirected(site_url());exit;?>
+
+<section id="posts">
+	<?php
+	   $posts=get-posts();
+	   foreach ($posts as $post ):	setup_post data($post);?>
+	   	<article>
+	   		<h2><?php the_title();?></h2>
+	   		<div></div>
+	   	</article>
+	   <?php endforeach;?>
+</section>
+
+	  
